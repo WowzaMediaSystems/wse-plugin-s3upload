8dd0b5b8f2975deeec01960dc8c08ec744c1abdf
Roger Littin <roger@wowza.com>
2016-02-10 12:29:22 +1300

    Updated instructions Updated .gitignore Updated package path and copyright text

---
+++ README.md
@@ -0,0 +1,45 @@
+# wse-plugin-s3upload
+
+S3 Upload is a Wowza Streaming Engine module will automatically upload completed recordings to an Amazon S3 bucket. It uses the AWS Java SDK to complete the uploads.
+
+## Prerequisites
+
+S3 Upload requires the AWS Java SDK to provide the upload functionality.  Download the [AWS SDK for Java](http://aws.amazon.com/sdk-for-java/), unzip it and copy `lib/aws-java-sdk-xx.xx.xx.jar` to your Wowza Streaming Engine `[install-dir]/lib/ folder`. 
+
+## Installation
+
+1. Copy `wse-plugin-s3upload.jar` to your Wowza Streaming Engine `[install-dir]/lib/`
+
+2. Add the following Module Definition to your Application configuration. See [Configure modules](http://www.wowza.com/forums/content.php?625-How-to-get-started-as-a-Wowza-Streaming-Engine-Manager-administrator#configModules) for details.
+
+Name | Description | Fully Qualified Class Name
+-----|-------------|---------------------------
+ModuleS3Upload | Upload recordings to Amazon S3. | com.wowza.wms.plugin.s3upload.ModuleS3Upload
+
+3. Adjust the default settings by adding the following properties to your application. See [Configure properties](http://www.wowza.com/forums/content.php?625-How-to-get-started-as-a-Wowza-Streaming-Engine-Manager-administrator#configProperties) for details.
+
+Path | Name | Type | Value | Notes
+-----|------|------|-------|------
+Root/Application | s3UploadAccessKey | String | `[your-s3-access-key]` | The S3 Access key for your AWS Account (default: not set).
+Root/Application | s3UploadSecretKey | String | `[your-s3-secret-key]` | The S3 Secret key for your AWS Account (default: not set).
+Root/Application | s3UploadBucketName | String | `[your-s3-bucket name]` | The S3 Bucket that you are going to upload the files to (default: not set).
+Root/Application | s3UploadResumeUploads | Boolean | `true` | Should S3 Upload resume any interrupted files after a restart (default: true).
+Root/Application | s3UploadDeletOriginalFiles | Boolean | `false` | Should S3 Upload delete the original files after uploading (default: false).
+
+## Usage
+
+When a recording is complete, a temporary file (recording-name.upload) is created to track the recording and sort any data that may be needed to resume the upload later if it's interrupted. The AWS **TransferManager** is then used to complete the upload, splitting the upload into a Multipart Upload if required. When the upload is complete, the temporary file is deleted. When the Wowza application starts or restarts, S3 Upload will check to see if there are any existing uploads that need to be completed. Any single part uploads that didn't originally complete will be restarted from the beginning and any Multipart uploads will be resumed from the last complete part. If the module is set to not resume uploads, any incomplete Multipart uploads will be deleted from the S3 bucket
+
+## API Reference
+
+[Wowza Streaming Engine Server-Side API Reference](http://www.wowza.com/resources/WowzaStreamingEngine_ServerSideAPI.pdf)
+
+## Contact
+Wowza Media Systems, LLC
+Wowza Media Systems provides developers with a platform to create streaming applications and solutions. See [Wowza Developer Tools](https://www.wowza.com/resources/developers) to learn more about our APIs and SDK.
+(Note this web page is being re-done and will be landing page for all developer resources).
+
+## License
+
+TODO: Add legal text here or LICENSE.txt file
+
---
+++ src/com/wowza/wms/plugin/s3upload/ModuleS3Upload.java
@@ -0,0 +1,284 @@
+/**
+ * Wowza server software and all components Copyright 2006 - 2016, Wowza Media Systems, LLC, licensed pursuant to the Wowza Media Software End User License Agreement.
+ */
+package com.wowza.wms.plugin.s3upload;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Map;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.event.ProgressEvent;
+import com.amazonaws.event.ProgressEventType;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.transfer.PersistableTransfer;
+import com.amazonaws.services.s3.transfer.PersistableUpload;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
+import com.amazonaws.services.s3.transfer.internal.S3ProgressListener;
+import com.wowza.util.StringUtils;
+import com.wowza.wms.application.*;
+import com.wowza.wms.logging.WMSLogger;
+import com.wowza.wms.logging.WMSLoggerFactory;
+import com.wowza.wms.logging.WMSLoggerIDs;
+import com.wowza.wms.module.*;
+import com.wowza.wms.stream.IMediaStream;
+import com.wowza.wms.stream.IMediaWriterActionNotify;
+
+public class ModuleS3Upload extends ModuleBase
+{
+	private class WriteListener implements IMediaWriterActionNotify
+	{
+
+		@Override
+		public void onWriteComplete(IMediaStream stream, File file)
+		{
+			String mediaName = file.getPath().replace(appInstance.getStreamStorageDir(), "");
+			if(mediaName.startsWith("/"))
+				mediaName = mediaName.substring(1);
+
+			if(transferManager == null)
+				return;
+			synchronized(lock)
+			{
+				File tmp = new File(file.getPath() + ".upload");
+				if (!tmp.exists())
+				{
+					try
+					{
+						tmp.createNewFile();
+					}
+					catch (IOException e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+			Upload upload = transferManager.upload(bucketName, mediaName, file);
+			upload.addProgressListener(new ProgressListener(mediaName));
+		}
+
+		@Override
+		public void onFLVAddMetadata(IMediaStream stream, Map<String, Object> extraMetadata)
+		{
+			// no-op
+		}
+	}
+	
+	private class ProgressListener implements S3ProgressListener
+	{
+		final String mediaName;
+		
+		ProgressListener(String mediaName)
+		{
+			this.mediaName = mediaName;
+		}
+
+		@Override
+		public void progressChanged(ProgressEvent progressEvent)
+		{
+			if(progressEvent.getEventType().isTransferEvent())
+			{
+				ProgressEventType type = progressEvent.getEventType();
+				System.out.println("progressChanged: " + type.toString());
+				switch (type)
+				{
+				case TRANSFER_COMPLETED_EVENT:
+					synchronized(lock)
+					{
+						File tmp = new File(appInstance.getStreamStorageDir(), mediaName + ".upload");
+						tmp.delete();
+					}
+					if(deleteOriginalFiles)
+					{
+						File file = new File(appInstance.getStreamStorageDir(), mediaName);
+						file.delete();
+					}
+					break;
+					
+				case TRANSFER_FAILED_EVENT:
+					logger.warn(MODULE_NAME + ".ProgerssListener [" + appInstance.getContextStr() + "/" + this.mediaName + "] transfer failed", WMSLoggerIDs.CAT_application, WMSLoggerIDs.EVT_comment);
+					break;
+					
+				default:
+					break;
+				}
+			}
+		}
+
+		@Override
+		public void onPersistableTransfer(final PersistableTransfer transfer)
+		{
+			System.out.println("onPersistableTransfer: " + transfer.serialize());
+			appInstance.getVHost().getThreadPool().execute(new Runnable() {
+
+				@Override
+				public void run()
+				{
+					synchronized(lock)
+					{
+						FileOutputStream fos = null;
+						File tmp = new File(appInstance.getStreamStorageDir(), mediaName + ".upload");
+						try
+						{
+							if (!tmp.exists())
+								tmp.createNewFile();
+							fos = new FileOutputStream(tmp);
+							transfer.serialize(fos);
+						}
+						catch (Exception e)
+						{
+
+						}
+						finally
+						{
+							if (fos != null)
+								try
+								{
+									fos.close();
+								}
+								catch (Exception e)
+								{
+								}
+						}
+					}
+				}
+			});
+		}
+		
+	}
+	
+	public static final String MODULE_NAME = "ModuleS3Upload";
+	public static final String PROP_NAME_PREFIX = "s3Upload";
+	
+	private WMSLogger logger = null;
+	private IApplicationInstance appInstance = null;
+	
+	private TransferManager transferManager = null;
+	private String accessKey = null;
+	private String secretKey = null;
+	private String bucketName = null;
+	
+	private boolean resumeUploads = true;
+	private boolean deleteOriginalFiles = false;
+	
+	private Object lock = new Object();
+
+	public void onAppStart(IApplicationInstance appInstance)
+	{
+		this.logger = WMSLoggerFactory.getLoggerObj(appInstance);
+		this.appInstance = appInstance;
+		
+		WMSProperties props = appInstance.getProperties();
+		this.accessKey = props.getPropertyStr("s3UploadAccessKey", this.accessKey);
+		this.secretKey = props.getPropertyStr("s3UploadSecretKey", this.secretKey);
+		this.bucketName = props.getPropertyStr("s3UploadBucketName", this.bucketName);
+		this.resumeUploads = props.getPropertyBoolean("s3UploadResumeUploads", this.resumeUploads);
+		this.deleteOriginalFiles = props.getPropertyBoolean("s3UploadDeletOriginalFiles", this.deleteOriginalFiles);
+		
+		if(StringUtils.isEmpty(accessKey) || StringUtils.isEmpty(secretKey))
+		{
+			logger.warn(MODULE_NAME + ".onAppStart: [" + appInstance.getContextStr() + "] missing S3 credentials", WMSLoggerIDs.CAT_application, WMSLoggerIDs.EVT_comment);
+			return;
+		}
+		
+		AmazonS3 s3Client = new AmazonS3Client(new BasicAWSCredentials(this.accessKey, this.secretKey));
+		
+		if (StringUtils.isEmpty(bucketName) || !s3Client.doesBucketExist(bucketName))
+		{
+			logger.warn(MODULE_NAME + ".onAppStart: [" + appInstance.getContextStr() + "] missing S3 bucket: " + bucketName, WMSLoggerIDs.CAT_application, WMSLoggerIDs.EVT_comment);
+			return;
+		}
+		
+		logger.info(MODULE_NAME + ".onAppStart [" + appInstance.getContextStr() + "] S3 Bucket Name: " + this.bucketName + ", Resume Uploads: " + this.resumeUploads + ", Delete Original Files: " + this.deleteOriginalFiles, WMSLoggerIDs.CAT_application, WMSLoggerIDs.EVT_comment);
+		transferManager = new TransferManager(s3Client);
+		resumeUploads();
+		
+		appInstance.addMediaWriterListener(new WriteListener());
+	}
+
+	public void onAppStop(IApplicationInstance appInstance)
+	{
+		if(transferManager != null)
+		{
+			transferManager.shutdownNow();
+		}
+	}
+
+	private void resumeUploads()
+	{
+		if(!resumeUploads)
+		{
+			transferManager.abortMultipartUploads(bucketName, new Date());
+			return;
+		}
+		
+		File storageDir = new File(appInstance.getStreamStorageDir());
+		File[] files = storageDir.listFiles(new FilenameFilter() {
+
+			@Override
+			public boolean accept(File dir, String name)
+			{
+				return name.toLowerCase().endsWith(".upload");
+			}
+		});
+		
+		for(File file : files)
+		{
+			String mediaName = file.getPath().replace(storageDir.getPath(), "");
+			if(mediaName.startsWith("/"))
+				mediaName = mediaName.substring(1);
+			
+			mediaName = mediaName.substring(0, mediaName.indexOf(".upload"));
+			Upload upload = null;
+			FileInputStream fis = null;
+			try
+			{
+				if(file.length() == 0)
+				{
+					File mediaFile = new File(storageDir, mediaName);
+					if(mediaFile.exists())
+					{
+						upload = transferManager.upload(bucketName, mediaName, mediaFile);
+					}
+					else
+					{
+						file.delete();
+					}
+				}
+				else
+				{
+					fis = new FileInputStream(file);
+					// Deserialize PersistableUpload information from disk.
+					PersistableUpload persistableUpload = PersistableTransfer.deserializeFrom(fis);
+					upload = transferManager.resumeUpload(persistableUpload);
+				}
+				if(upload != null)
+					upload.addProgressListener(new ProgressListener(mediaName));
+			}
+			catch (Exception e)
+			{
+				logger.error(MODULE_NAME + ".resumeUploads error resuming upload: [" + appInstance.getContextStr() + "/" + file.getName() + "]", e);
+			}
+			finally
+			{
+				if(fis != null)
+				{
+					try
+					{
+						fis.close();
+					}
+					catch (IOException e)
+					{
+					}
+				}
+			}
+		}
+	}
+}
