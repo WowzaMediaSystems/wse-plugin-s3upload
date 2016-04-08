bb1eabc42fbea91762006ead3021c2392f112413
Roger Littin <roger@wowza.com>
2016-04-08 13:02:42 +1200

    Updated copyright text in source code files

--- src/com/wowza/wms/plugin/s3upload/ModuleS3Upload.java
+++ src/com/wowza/wms/plugin/s3upload/ModuleS3Upload.java
@@ -1,5 +1,6 @@
 /**
- * Wowza server software and all components Copyright 2006 - 2016, Wowza Media Systems, LLC, licensed pursuant to the Wowza Media Software End User License Agreement.
+ * This code and all components (c) Copyright 2006 - 2016, Wowza Media Systems, LLC.  All rights reserved.
+ * This code is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
  */
 package com.wowza.wms.plugin.s3upload;
 
